cfb1cf77e6a56ab8ae68d5792831488f0ef95589
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-06-22 13:54:38 +0900

    '更新'

---
+++ src/pages/ItemInfo/Type/itemInfoType.ts
@@ -0,0 +1,16 @@
+
+export type toppingFlagType = {
+    [key: string]:boolean
+}
+
+export type newToppingSizeType = {
+    [key: string]:number
+}
+
+export type toppingWillAddType = {
+    [key: string]:number
+}
+
+export type sendItemType = {
+    [key:string]:number|toppingWillAddType[];
+}
